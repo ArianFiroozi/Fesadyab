a44a06d61eb39bf4ad2dad155ea005b6218352b5
ArianFiroozi <arianfiroozi3@gmail.com>
2024-11-12 20:30:22 +0330

    add: detectFraudulentTransaction test

--- src/test/java/domain/TransactionEngineTest.java
+++ src/test/java/domain/TransactionEngineTest.java
@@ -29,4 +29,53 @@ public class TransactionEngineTest {
         transactionEngine.addTransactionAndDetectFraud(transaction);
         Assertions.assertEquals(0, transactionEngine.addTransactionAndDetectFraud(transaction));
     }
+
+    @Test
+    void detectFraudulentTransaction_transactionNotDebit_returnsZero() {
+        Transaction transaction=new Transaction();
+        transaction.setTransactionId(1);
+        transaction.setAccountId(2);
+        transaction.setAmount(100);
+        transaction.setDebit(false);
+
+        TransactionEngine transactionEngine=new TransactionEngine();
+
+        Assertions.assertEquals(0, transactionEngine.detectFraudulentTransaction(transaction));
+    }
+
+    @Test
+    void detectFraudulentTransaction_transactionHasExcessiveDebit_returnsZero() {
+        Transaction transaction=new Transaction();
+        transaction.setTransactionId(1);
+        transaction.setAccountId(2);
+        transaction.setAmount(1200);
+        transaction.setDebit(true);
+
+        Transaction fakeTransaction=new Transaction();
+        fakeTransaction.setTransactionId(1);
+        fakeTransaction.setAccountId(2);
+        fakeTransaction.setAmount(1200);
+        fakeTransaction.setDebit(true);
+
+        TransactionEngine transactionEngine=new TransactionEngine();
+        transactionEngine.addTransactionAndDetectFraud(transaction);
+        fakeTransaction.setAmount(2500);
+
+        Assertions.assertEquals(100, transactionEngine.detectFraudulentTransaction(fakeTransaction));
+    }
+
+
+    @Test
+    void detectFraudulentTransaction_transactionIsCorrect_returnsZero() {
+        Transaction transaction=new Transaction();
+        transaction.setTransactionId(1);
+        transaction.setAccountId(2);
+        transaction.setAmount(1200);
+        transaction.setDebit(true);
+
+        TransactionEngine transactionEngine=new TransactionEngine();
+        transactionEngine.addTransactionAndDetectFraud(transaction);
+
+        Assertions.assertEquals(0, transactionEngine.detectFraudulentTransaction(transaction));
+    }
 }
