40b43553577f2bf8392a2643d064fb83e21786f9
ArianFiroozi <arianfiroozi3@gmail.com>
2024-11-12 19:50:07 +0330

    add: transaction tests

--- src/test/java/domain/TransactionEngineTest.java
+++ src/test/java/domain/TransactionEngineTest.java
@@ -1,10 +1,47 @@
 package domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TransactionEngineTest {
-    @Test
-    void test_test() {
+    private Transaction firstTransaction;
 
+    @BeforeEach
+    void setup() {
+        firstTransaction=new Transaction();
+        firstTransaction.setTransactionId(1);
+        firstTransaction.setAccountId(2);
+        firstTransaction.setAmount(100);
+        firstTransaction.setDebit(true);
+    }
+
+    @Test
+    void equals_transactionsAreEqual_returnsTrue() {
+        Transaction secondTransaction=new Transaction();
+        secondTransaction.setTransactionId(firstTransaction.getTransactionId());
+        secondTransaction.setAccountId(firstTransaction.getAccountId());
+        secondTransaction.setAmount(firstTransaction.getAmount());
+        secondTransaction.setDebit(firstTransaction.isDebit());
+
+        Assertions.assertTrue(firstTransaction.equals(secondTransaction));
+    }
+
+    @Test
+    void equals_transactionsHaveDifferentID_returnsFalse() {
+        Transaction secondTransaction=new Transaction();
+        secondTransaction.setTransactionId(firstTransaction.getTransactionId()+1);
+        secondTransaction.setAccountId(firstTransaction.getAccountId());
+        secondTransaction.setAmount(firstTransaction.getAmount());
+        secondTransaction.setDebit(firstTransaction.isDebit());
+
+        Assertions.assertFalse(firstTransaction.equals(secondTransaction));
+    }
+
+    @Test
+    void equals_otherObjectIsNotTransaction_returnsFalse() {
+        int intObject=1;
+
+        Assertions.assertFalse(firstTransaction.equals(intObject));
     }
 }
