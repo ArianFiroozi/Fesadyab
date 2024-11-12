50b9e00a6b9d51897e1dcab977e4224fa5b41cf5
ArianFiroozi <arianfiroozi3@gmail.com>
2024-11-12 20:15:28 +0330

    add: addTransactionAndDetectFraud tests

--- src/test/java/domain/TransactionEngineTest.java
+++ src/test/java/domain/TransactionEngineTest.java
@@ -1,10 +1,32 @@
 package domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TransactionEngineTest {
     @Test
-    void addTransactionAndDetectFraud_correctTransactionWithoutFraud_addsTransactionAndReturnsZero() {
+    void addTransactionAndDetectFraud_correctTransactionWithFraud_addsTransactionAndReturnsFraudScore() {
+        Transaction transaction=new Transaction();
+        transaction.setTransactionId(1);
+        transaction.setAccountId(2);
+        transaction.setAmount(100);
+        transaction.setDebit(true);
 
+        TransactionEngine transactionEngine=new TransactionEngine();
+
+        Assertions.assertEquals(100, transactionEngine.addTransactionAndDetectFraud(transaction));
+        Assertions.assertTrue(transactionEngine.transactionHistory.contains(transaction));
+    }
+    @Test
+    void addTransactionAndDetectFraud_repeatedTransaction_returnsZero() {
+        Transaction transaction=new Transaction();
+        transaction.setTransactionId(1);
+        transaction.setAccountId(2);
+        transaction.setAmount(100);
+        transaction.setDebit(true);
+
+        TransactionEngine transactionEngine=new TransactionEngine();
+        transactionEngine.addTransactionAndDetectFraud(transaction);
+        Assertions.assertEquals(0, transactionEngine.addTransactionAndDetectFraud(transaction));
     }
 }
