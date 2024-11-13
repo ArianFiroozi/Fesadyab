959b15861f171e0048edfe2b07a86c7769710512
ArianFiroozi <arianfiroozi3@gmail.com>
2024-11-13 07:35:54 +0330

    refactor: transactionEngineTest code cleaning

--- src/test/java/domain/TransactionEngineTest.java
+++ src/test/java/domain/TransactionEngineTest.java
@@ -1,65 +1,50 @@
 package domain;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TransactionEngineTest {
-    @Test
-    void addTransactionAndDetectFraud_correctTransactionWithFraud_addsTransactionAndReturnsFraudScore() {
-        Transaction transaction=new Transaction();
+    private TransactionEngine transactionEngine;
+    private Transaction transaction;
+
+    @BeforeEach
+    void setup() {
+        transactionEngine=new TransactionEngine();
+        transaction=new Transaction();
         transaction.setTransactionId(1);
         transaction.setAccountId(2);
         transaction.setAmount(100);
         transaction.setDebit(true);
+    }
 
-        TransactionEngine transactionEngine=new TransactionEngine();
-
+    @Test
+    void addTransactionAndDetectFraud_correctTransactionWithFraud_addsTransactionAndReturnsFraudScore() {
         Assertions.assertEquals(100, transactionEngine.addTransactionAndDetectFraud(transaction));
         Assertions.assertTrue(transactionEngine.transactionHistory.contains(transaction));
     }
     @Test
     void addTransactionAndDetectFraud_repeatedTransaction_returnsZero() {
-        Transaction transaction=new Transaction();
-        transaction.setTransactionId(1);
-        transaction.setAccountId(2);
-        transaction.setAmount(100);
-        transaction.setDebit(true);
-
-        TransactionEngine transactionEngine=new TransactionEngine();
         transactionEngine.addTransactionAndDetectFraud(transaction);
         Assertions.assertEquals(0, transactionEngine.addTransactionAndDetectFraud(transaction));
     }
 
     @Test
     void detectFraudulentTransaction_transactionNotDebit_returnsZero() {
-        Transaction transaction=new Transaction();
-        transaction.setTransactionId(1);
-        transaction.setAccountId(2);
-        transaction.setAmount(100);
         transaction.setDebit(false);
-
-        TransactionEngine transactionEngine=new TransactionEngine();
 
         Assertions.assertEquals(0, transactionEngine.detectFraudulentTransaction(transaction));
     }
 
     @Test
     void detectFraudulentTransaction_transactionHasExcessiveDebit_returnsZero() {
-        Transaction transaction=new Transaction();
-        transaction.setTransactionId(1);
-        transaction.setAccountId(2);
-        transaction.setAmount(1200);
-        transaction.setDebit(true);
-
         Transaction fakeTransaction=new Transaction();
         fakeTransaction.setTransactionId(1);
         fakeTransaction.setAccountId(2);
-        fakeTransaction.setAmount(1200);
+        fakeTransaction.setAmount(300);
         fakeTransaction.setDebit(true);
 
-        TransactionEngine transactionEngine=new TransactionEngine();
         transactionEngine.addTransactionAndDetectFraud(transaction);
-        fakeTransaction.setAmount(2500);
 
         Assertions.assertEquals(100, transactionEngine.detectFraudulentTransaction(fakeTransaction));
     }
@@ -67,13 +52,6 @@ public class TransactionEngineTest {
 
     @Test
     void detectFraudulentTransaction_transactionIsCorrect_returnsZero() {
-        Transaction transaction=new Transaction();
-        transaction.setTransactionId(1);
-        transaction.setAccountId(2);
-        transaction.setAmount(1200);
-        transaction.setDebit(true);
-
-        TransactionEngine transactionEngine=new TransactionEngine();
         transactionEngine.addTransactionAndDetectFraud(transaction);
 
         Assertions.assertEquals(0, transactionEngine.detectFraudulentTransaction(transaction));
