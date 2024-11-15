bbc0e605177d7abe6b34aabb1916c0cdcc308d35
ArianFiroozi <arianfiroozi3@gmail.com>
2024-11-15 19:04:41 +0330

    full coverage

--- src/test/java/domain/TransactionEngineTest.java
+++ src/test/java/domain/TransactionEngineTest.java
@@ -49,11 +49,70 @@ public class TransactionEngineTest {
         Assertions.assertEquals(100, transactionEngine.detectFraudulentTransaction(fakeTransaction));
     }
 
-
     @Test
     void detectFraudulentTransaction_transactionIsCorrect_returnsZero() {
         transactionEngine.addTransactionAndDetectFraud(transaction);
 
         Assertions.assertEquals(0, transactionEngine.detectFraudulentTransaction(transaction));
+    }
+
+    @Test
+    void getTransactionPatternAboveThreshold_patternAboveThreshold_returnsDiff() {
+        Transaction transaction2=new Transaction();
+        transaction2.setAccountId(2);
+        transaction2.setAmount(1200);
+
+        transactionEngine.addTransactionAndDetectFraud(transaction);
+        transactionEngine.addTransactionAndDetectFraud(transaction2);
+
+        Assertions.assertEquals(1100, transactionEngine.getTransactionPatternAboveThreshold(1000));
+    }
+
+    @Test
+    void getTransactionPatternAboveThreshold_patternUnderThreshold_returnsZero() {
+        Transaction transaction2=new Transaction();
+        transaction2.setAccountId(2);
+        transaction2.setAmount(1200);
+
+        transactionEngine.addTransactionAndDetectFraud(transaction);
+        transactionEngine.addTransactionAndDetectFraud(transaction2);
+
+        Assertions.assertEquals(0, transactionEngine.getTransactionPatternAboveThreshold(1200));
+    }
+
+    @Test
+    void getTransactionPatternAboveThreshold_multipleTransactionsAboveThreshold_returnsZero() {
+        Transaction transaction2=new Transaction();
+        transaction2.setAccountId(2);
+        transaction2.setTransactionId(2);
+        transaction2.setAmount(1200);
+        Transaction transaction3=new Transaction();
+        transaction3.setTransactionId(3);
+        transaction2.setAccountId(3);
+        transaction3.setAmount(1900);
+
+        transactionEngine.addTransactionAndDetectFraud(transaction);
+        transactionEngine.addTransactionAndDetectFraud(transaction2);
+        transactionEngine.addTransactionAndDetectFraud(transaction3);
+
+        Assertions.assertEquals(0, transactionEngine.getTransactionPatternAboveThreshold(300));
+    }
+
+    @Test
+    void getTransactionPatternAboveThreshold_multipleSamePatternTransactionsAboveThreshold_returnsZero() {
+        Transaction transaction2=new Transaction();
+        transaction2.setAccountId(2);
+        transaction2.setTransactionId(2);
+        transaction2.setAmount(1200);
+        Transaction transaction3=new Transaction();
+        transaction3.setTransactionId(3);
+        transaction2.setAccountId(3);
+        transaction3.setAmount(2300);
+
+        transactionEngine.addTransactionAndDetectFraud(transaction);
+        transactionEngine.addTransactionAndDetectFraud(transaction2);
+        transactionEngine.addTransactionAndDetectFraud(transaction3);
+
+        Assertions.assertEquals(1100, transactionEngine.getTransactionPatternAboveThreshold(300));
     }
 }
